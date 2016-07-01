dcdd11f9ec51e05536325e9eeb1292ba9c2238be
Cypresslin <Cypresslin@users.noreply.github.com>
2016-07-01 19:29:24 +0800

    Add internet connection watcher feature
    Re-design the internet connection watch feature to use strace.
    Modify the layout for watcher page.

--- utils/internet_watcher.py
+++ utils/internet_watcher.py
@@ -1,18 +1,45 @@
 #!/usr/bin/env python3
 '''
+Script for monitoring network connections.
+
 Author: Po-Hsu Lin <po-hsu.lin@canonical.com>
 '''
-import subprocess
 
-home_dir = '/home/phablet'
+import argparse
+import datetime
+import re
+import subprocess
+import sys
+
+parser = argparse.ArgumentParser(description='Network connection monitor')
+parser.add_argument('--app', help='Target app', required=True)
+args = parser.parse_args()
+
 try:
-    process = subprocess.Popen(['adb', 'shell', 'sudo', './nethogs', 'wlan0'], stdout=subprocess.PIPE)
-    while True:
-        output = process.stdout.readline().decode('utf-8')
-        if output == '' and process.poll() is not None:
-            break
-        else:
-            print(output.strip())
+    proc_name = args.app
+    proc_id = subprocess.check_output(['adb', 'shell', 'ubuntu-app-pid', proc_name]).decode('utf-8').rstrip()
+    if proc_id.isnumeric():
+        # Supressor list, get rid of 127.0.0.1 and 127.0.1.1 as well
+        supressor = ['bind(', 'recv(', 'send(', 'socket(', 'getsockopt(', 'recvmsg(', 'setsockopt(', '127.0.0.1', '127.0.1.1']
+        # Kill the old strace task first, targeted on internet watcher process
+        process = subprocess.check_output(['adb', 'shell', 'sudo', 'pkill', '-f', 'trace=network'])
+        process = subprocess.Popen(['adb', 'shell', 'sudo', 'strace', '-f', '-e', 'trace=network', '-p', proc_id], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        while True:
+            output = process.stdout.readline().decode('utf-8').strip()
+            if output == '' and process.poll() is not None:
+                break
+            else:
+                # Reformat the output, get rid of the [pid #####]
+                output = re.sub('^\[pid\s+\d+\] ', '', output)
+                if not any(mute in output for mute in supressor):
+                    # focus on address
+                    if 'addr' in output:
+                        timestamp='{:%Y%m%d %H:%M:%S}'.format(datetime.datetime.now())
+                        print(timestamp, '-', output)
+                        sys.stdout.flush()
+    else:
+        print(proc_name, "is not running")
+
 except KeyboardInterrupt:
     print("Process Terminated by user")
 
