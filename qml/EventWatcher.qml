09e4b4787768c8e04c42fc7f8548e8fb36632d87
Cypresslin <Cypresslin@users.noreply.github.com>
2016-07-06 22:45:38 +0800

    Add location service monitoring feature
    Implement location service monitoring feature by using strace.
    Add the corresponding event monitoring page.

---
+++ qml/EventWatcher.qml
@@ -0,0 +1,59 @@
+import QtQuick 2.0
+import Process 1.0
+import "colour.js" as Colour
+import QtQuick.Dialogs 1.2
+import Ubuntu.Components 1.3
+import QtQuick.Layouts 1.2
+
+
+Item {
+    Component.onCompleted: {
+        console.log('Event Watcher loaded')
+          cmd_event.start(applicationDirPath + '/utils/event_watcher.py', ['--app', appNameLabel.text])
+    }
+    Process {
+        id: cmd_event
+        onReadyRead: {
+            var string = readAll()
+            eventLog.text += string
+            console.log(eventLog.text)
+        }
+    }
+
+    Column {
+        id: mainCol
+        anchors { 
+            fill: parent
+            margins: 30
+            
+            horizontalCenter: parent.horizontalCenter
+        }
+        spacing: units.gu(2)
+        Row {
+            anchors {
+                horizontalCenter: parent.horizontalCenter
+             }
+            Text {
+                text: i18n.tr('Senitive Event Monitor')
+                font.pointSize: 16
+            }
+        }
+        Row {
+            Flickable {
+                contentHeight: eventLog.contentHeight
+                width: mainCol.width
+                height: 500
+                clip: true
+
+                TextEdit {
+                    id: eventLog
+                    anchors.fill: parent
+                    font.pointSize: 10
+                    selectionColor: Colour.palette['Green']
+                    wrapMode: TextEdit.WordWrap
+                    cursorPosition: eventLog.text.length
+                }
+            }
+        }
+    }
+}
---
+++ utils/event_watcher.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+'''
+Script for monitoring sensitive events.
+
+Author: Po-Hsu Lin <po-hsu.lin@canonical.com>
+'''
+
+import argparse
+import datetime
+import re
+import subprocess
+import sys
+
+parser = argparse.ArgumentParser(description='Sensitive event monitor')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--app', help='Targeted app')
+args = parser.parse_args()
+
+try:
+    proc_name = args.app
+    proc_id = subprocess.check_output(['adb', 'shell', 'ubuntu-app-pid', proc_name]).decode('utf-8').rstrip()
+    if proc_id.isnumeric():
+        # Try to kill strace process first
+        output = subprocess.check_output(['adb', 'shell', 'sudo', 'pkill', '-f', 'strace'])
+        process = subprocess.Popen(['adb', 'shell', 'sudo', 'strace', '-f', '-e', 'trace=sendmsg,recvmsg', '-s', '400' , '-p', proc_id], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        while True:
+            output = process.stdout.readline().decode('utf-8').strip()
+            if output == '' and process.poll() is not None:
+                break
+            else:
+                # Print only when it's location.Service related
+                if "location.Service" in output:
+                    # Reformat the output, extract events and remove human non-readable parts:
+                    # Remove [pid ####]
+                    output = re.sub('^\[pid\s+\d+\] ', '', output)
+                    # Remove msg_* and MSG_*
+                    output = re.sub('msg_\w+(\(\d\))*=*\w+', '', output, re.S ,re.I)
+                    # Rename and Relocate com.ubuntu.location.Service
+                    output = re.sub('com.ubuntu.location.Service', '', output)
+                    output = 'LocationService: ' + output
+                    # Filter message by parsing meaningful strings
+                    words = re.findall('[a-z][a-z]{2,}', output, re.I)
+                    output = ' '.join(words)
+                    
+                    # output here
+                    timestamp='{:%Y%m%d %H:%M:%S}'.format(datetime.datetime.now())
+                    print(timestamp, '-', output)
+                    sys.stdout.flush()
+    else:
+        print(proc_name, "is not running")
+
+except KeyboardInterrupt:
+    print("Process Terminated by user")
+except Exception as e:
+    print("Exception occurred - {}".format(e))
