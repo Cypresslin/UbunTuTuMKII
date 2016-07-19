2d53d2ad2a7da59da96833413afc097ed6f3d769
Cypresslin <Cypresslin@users.noreply.github.com>
2016-07-20 02:47:45 +0800

    Reduce adb calls in list_app script
    Improve the performance of the list_app script, reduce adb calls.
    Time cost drop from 13 sec to 3 sec.

--- utils/list_app.py
+++ utils/list_app.py
@@ -15,6 +15,7 @@ Authors:
 from gettext import gettext as _
 import argparse
 import json
+import re
 import subprocess
 import sys
 import time
@@ -30,82 +31,68 @@ group.add_argument('--list', action='store_true',
 
 args = parser.parse_args()
 
+
 try:
     if args.list:
         app_dict = {}
-        # Get the exectuable Legacy app list (it's the title as well)
-        cmd = ['adb', 'shell', 'grep', '-l', 'X-Ubuntu-Touch=true',
-               '/usr/share/applications/*.desktop']
-        fn_legacy = subprocess.check_output(cmd).decode('utf8')
-        fn_legacy = fn_legacy.split()
-        # Exclude file that with "OnlyShowIn=Old" and "NoDisplay=true" key
-        cmd = ['adb', 'shell', 'grep', '-l', 'OnlyShowIn=Old',
-               '/usr/share/applications/*']
-        excludes = subprocess.check_output(cmd).decode('utf8')
-        cmd = ['adb', 'shell', 'grep', '-l', 'NoDisplay=true',
-               '/usr/share/applications/*']
-        excludes += subprocess.check_output(cmd).decode('utf8')
-        excludes = excludes.split()
-        for exc in excludes:
-            if exc in fn_legacy:
-                fn_legacy.remove(exc)
-        # Get info for legacy apps and put them into a dictionary
-        for fn in fn_legacy:
-            # Reformat the executable name
-            app_exec = fn.replace('/usr/share/applications/', '')
-            app_exec = app_exec.replace('.desktop', '')
-            # Get the name in Name[zh_CN] first, if not available, use the Name instead
-            cmd = ['adb', 'shell', 'grep', r'^Name\[zh_CN\]=', fn]
-            app_name = subprocess.check_output(cmd).decode('utf8').rstrip()
-            if app_name:
-                app = app_name.split('=')[1]
-            else:
-                cmd = ['adb', 'shell', 'grep', '^Name=', fn]
-                app_name = subprocess.check_output(cmd).decode('utf8').rstrip()
-                app = app_name.split('=')[1]
-            cmd = ['adb', 'shell', 'dpkg', '-s', app_exec, '|', 'grep',
-                   '-e', 'Version', '-e', 'Maintainer']
-            info = subprocess.check_output(cmd).decode('utf8').rstrip()
-            contact, ver = info.split('\r\n')
-            contact = contact.split(': ')[1]
-            ver = ver.split(': ')[1].split('+')[0]
-            app_dict[app] = {'ver': ver, 'info': contact, 'exec': app_exec}
+        # Get all Legacy app desktop file content
+        cmd = ['adb', 'shell', 'grep', '', '/usr/share/applications/*.desktop']
+        output = subprocess.check_output(cmd).decode('utf8')
+        output = output.split('[Desktop Entry]')
+        # Exclude app that contains 'OnlyShowIn=Old' and 'NoDisplay=true'
+        exclude = ['OnlyShowIn=Old', 'NoDisplay=true']
+        # Include app that contains 'X-Ubuntu-Touch=true'
+        for item in output:
+            if 'X-Ubuntu-Touch=true' in item:
+                if all(pattern not in item for pattern in exclude):
+                    # Get info for legacy apps and put them into a dictionary
+                    regex = '(?P<file_name>.+):'
+                    fn = re.search(regex, item).group('file_name')
+                    app_exec = fn.replace('/usr/share/applications/', '')
+                    app_exec = app_exec.replace('.desktop', '')
+                    # Get the name in Name[zh_CN] first, if not available, use the Name instead
+                    if 'Name[zh_CN]=' in item:
+                        regex = r'Name\[zh_CN\]=(?P<app_name>.+)'
+                    else:
+                        regex = 'Name=(?P<app_name>.+)'
+                    app_name = re.search(regex, item).group('app_name').strip('\r')
+                    # Get the version, maintainer information here from dpkg
+                    cmd = ['adb', 'shell', 'dpkg', '-s', app_exec, '|', 'grep',
+                           '-e', 'Version', '-e', 'Maintainer']
+                    info = subprocess.check_output(cmd).decode('utf8').rstrip()
+                    contact, ver = info.split('\r\n')
+                    contact = contact.split(': ')[1]
+                    ver = ver.split(': ')[1].split('+')[0]
+                    app_dict[app_name] = {'ver': ver, 'info': contact, 'exec': app_exec}
 
         # Get the complete info of Click app from manifest
         cmd = ['adb', 'shell', 'click', 'list', '--manifest']
         data = subprocess.check_output(cmd).decode('utf8')
         data = json.loads(data)
-        # Get the exectuable Click app list
-        cmd = ['adb', 'shell', 'grep', '-l', 'X-Ubuntu-Touch=true',
-               '/home/phablet/.local/share/applications/*.desktop']
-        fn_click = subprocess.check_output(cmd).decode('utf8')
-        fn_click = fn_click.split()
-
-        # Put app name into a dictionary for later query
-        app_click = {}
-        for fn in fn_click:
-            # Reformat the executable name
-            app_exec = fn.replace('/home/phablet/.local/share/applications/', '')
-            app_exec = app_exec.replace('.desktop', '')
-            # Get the name in Name[zh_CN] first, if not available, use the Name instead
-            cmd = ['adb', 'shell', 'grep', r'^Name\[zh_CN\]=', fn]
-            app_name = subprocess.check_output(cmd).decode('utf8').rstrip()
-            if app_name:
-                app = app_name.split('=')[1]
-            else:
-                cmd = ['adb', 'shell', 'grep', '^Name=', fn]
-                app_name = subprocess.check_output(cmd).decode('utf8').rstrip()
-                app = app_name.split('=')[1]
-            app_click[app_exec] = app
-        # Reorganize information and combine with current dictionary
-        for item in data:
-            if any(item['name'] in app for app in app_click):
-                for i, executable in enumerate(app_click):
-                    if item['name'] in executable:
-                        app_dict[app_click[executable]] = {
-                            'ver': item['version'],
-                            'info': item['maintainer'],
-                            'exec': executable}
+        # Get all Click app desktop file content
+        cmd = ['adb', 'shell', 'grep', '', '/home/phablet/.local/share/applications/*.desktop']
+        output = subprocess.check_output(cmd).decode('utf8')
+        output = output.split('[Desktop Entry]')
+        # Include app that contains 'X-Ubuntu-Touch=true'
+        for item in output:
+            if 'X-Ubuntu-Touch=true' in item:
+                regex = '(?P<file_name>.+):'
+                fn = re.search(regex, item).group('file_name')
+                app_exec = fn.replace('/home/phablet/.local/share/applications/', '')
+                app_exec = app_exec.replace('.desktop', '')
+                # Get the name in Name[zh_CN] first, if not available, use the Name instead
+                if 'Name[zh_CN]=' in item:
+                    regex = r'Name\[zh_CN\]=(?P<app_name>.+)'
+                else:
+                    regex = 'Name=(?P<app_name>.+)'
+                app_name = re.search(regex, item).group('app_name').strip('\r')
+                # Get the version, maintainer information here from manifest
+                for entry in data:
+                    if entry['name'] in app_exec:
+                        app_dict[app_name] = {
+                            'ver': entry['version'],
+                            'info': entry['maintainer'],
+                            'exec': app_exec}
                         break
 
         # Return app titles and version here for QML combobox
